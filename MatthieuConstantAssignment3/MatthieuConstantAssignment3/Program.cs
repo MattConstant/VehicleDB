1b10903592c1a596e3c88aa5a3c1a192ecf0e1f0
Matthieu Constant <91146150+MattConstant@users.noreply.github.com>
2022-08-04 12:52:41 -0400

    Added functions
    Added functions for the CRUD

--- MatthieuConstantAssignment3/MatthieuConstantAssignment3/Program.cs
+++ MatthieuConstantAssignment3/MatthieuConstantAssignment3/Program.cs
@@ -20,9 +20,6 @@ namespace MatthieuConstantAssignment3
        
     }
 
-
-
-
     public class CarManagement
     {
 
@@ -46,6 +43,7 @@ namespace MatthieuConstantAssignment3
             {
                 while (option > 4 || option <= 0)
                 {
+                    Console.WriteLine("");
                     Console.WriteLine("Car Manager");
                     Console.WriteLine("Please Chose an option");
                     Console.WriteLine("Enter 1 for Vehicle menu");
@@ -195,6 +193,7 @@ namespace MatthieuConstantAssignment3
                     }
 
                     conn.Close();
+                    Init();
                 }
                 catch (Exception e)
                 {
@@ -204,7 +203,6 @@ namespace MatthieuConstantAssignment3
             
             void addVehicle()
             {
-                //add a new vehicles to the database
                 
                 try
                 {
@@ -212,6 +210,7 @@ namespace MatthieuConstantAssignment3
                     string cs = GetConnectionString("CarDBMdf");
                     Random rnd = new Random();
                     int newId = rnd.Next(11, 99);
+                    Console.WriteLine("");
                     Console.WriteLine("Enter the make of the vehicle");
                     string make = Console.ReadLine();
                     Console.WriteLine("Enter the model of the vehicle");
@@ -237,10 +236,19 @@ namespace MatthieuConstantAssignment3
                 int result = cmd.ExecuteNonQuery();
                         conn.Close();
                         if (result == 1)
-                    Console.WriteLine("Employee inserted");
-                else
-                    Console.WriteLine("Employee not inserted");
-                       
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Vehicle inserted");
+                            Init();
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Vehicle not inserted");
+                            Init();
+
+                        }
+
                     }
                     
                 }
@@ -254,53 +262,407 @@ namespace MatthieuConstantAssignment3
 
             void updateVehicle()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the id of the vehicle you wish to update");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the make of the vehicle");
+                    string make = Console.ReadLine();
+                    Console.WriteLine("Enter the model of the vehicle");
+                    string model = Console.ReadLine();
+                    Console.WriteLine("Enter the year of the vehicle");
+                    string year = Console.ReadLine();
+                    Console.WriteLine("Enter if the vehicle is new or used");
+                    string newUsed = Console.ReadLine();
+                    string query = "Update Vehicles set make = @make, model = @model, year = @year, newUsed = @newUsed where id = @id";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("make", make);
+                        cmd.Parameters.AddWithValue("model", model);
+                        cmd.Parameters.AddWithValue("year", year);
+                        cmd.Parameters.AddWithValue("newUsed", newUsed);
+                        cmd.Parameters.AddWithValue("id", id);
 
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Vehicle updated");
+                            Init();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Vehicle not updated");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
 
             void deleteVehicle()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the id of the vehicle you wish to delete");
+                    int id = int.Parse(Console.ReadLine());
+                    string query = "Delete from Vehicles where id = @id";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("id", id);
 
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Vehicle deleted");
+                            Init();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Vehicle not deleted");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             //-----------------Inventory Functions-----------------
 
             void addInventory()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Random rnd = new Random();
+                    int newId = rnd.Next(11, 99);
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the number on hand");
+                    string numberOnHand = Console.ReadLine();
+                    Console.WriteLine("Enter the price");
+                    string price = Console.ReadLine();
+                    Console.WriteLine("Enter the cost");
+                    string cost = Console.ReadLine();
+                    string query = "Insert into Inventory (id, numberonHand, price, cost) values (@id, @numberonHand, @price, @cost)";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("numberonHand", numberOnHand);
+                        cmd.Parameters.AddWithValue("price", price);
+                        cmd.Parameters.AddWithValue("cost", cost);
+                        cmd.Parameters.AddWithValue("id", newId);
+
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Inventory inserted");
+                            Init();
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Inventory not inserted");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
 
             }
 
             void viewInventory()
             {
-
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    string query = "Select * from Inventory";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Id: " + reader["id"]);
+                            Console.WriteLine("Number on hand: " + reader["numberonHand"]);
+                            Console.WriteLine("Price: " + reader["price"]);
+                            Console.WriteLine("Cost: " + reader["cost"]);
+                        }
+                        conn.Close();
+                    }
+                    Init();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
-            
+
             void deleteInventory()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the id of the item you wish to delete");
+                    int id = int.Parse(Console.ReadLine());
+                    string query = "Delete from Inventory where id = @id";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("id", id);
 
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Inventory deleted");
+                            Init();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Inventory not deleted");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
 
             void updateInventory()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the id of the item you wish to update");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the number on hand");
+                    string numberOnHand = Console.ReadLine();
+                    Console.WriteLine("Enter the price");
+                    string price = Console.ReadLine();
+                    Console.WriteLine("Enter the cost");
+                    string cost = Console.ReadLine();
+                    string query = "Update Inventory set numberonHand = @numberonHand, price = @price, cost = @cost where id = @id";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("numberonHand", numberOnHand);
+                        cmd.Parameters.AddWithValue("price", price);
+                        cmd.Parameters.AddWithValue("cost", cost);
+                        cmd.Parameters.AddWithValue("id", id);
 
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Inventory updated");
+                            Init();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Inventory not updated");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             //------------------Repair Functions-------------------
-            
+
             void viewRepairs()
             {
-
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    string query = "Select * from Repairs";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine(reader["id"] +" "+ "What to repair: " + reader["whatToRepair"]);
+                        }
+                        conn.Close();
+                    }
+                    Init();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
 
             void addRepair()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Random rnd = new Random();
+                    int newId = rnd.Next(11, 99);
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the what to repair");
+                    string whatToRepair = Console.ReadLine();
+                    string query = "Insert into Repairs (id, whatToRepair) values (@id, @whatToRepair)";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("whatToRepair", whatToRepair);
+                        cmd.Parameters.AddWithValue("id", newId);
+
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Repair inserted");
+                            Init();
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Repair not inserted");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
 
             }
 
             void updateRepair()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the id of the repair you wish to update");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the what to repair");
+                    string whatToRepair = Console.ReadLine();
+                    string query = "Update Repairs set whatToRepair = @whatToRepair where id = @id";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("whatToRepair", whatToRepair);
+                        cmd.Parameters.AddWithValue("id", id);
 
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Repair updated");
+                            Init();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Repair not updated");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
 
             void deleteRepair()
             {
+                try
+                {
+                    string cs = GetConnectionString("CarDBMdf");
+                    Console.WriteLine("");
+                    Console.WriteLine("Enter the id of the repair you wish to delete");
+                    int id = int.Parse(Console.ReadLine());
+                    string query = "Delete from Repairs where id = @id";
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("id", id);
+
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        if (result == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Repair deleted");
+                            Init();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Repair not deleted");
+                            Init();
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
 
             }
 
